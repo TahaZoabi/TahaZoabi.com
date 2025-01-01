d3772f38ab8fa743d33ad3b47d3f0e62405a9f47
Taha Zoabi <zoabi03@gmail.com>
2025-01-01 10:48:52 +0200

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
+import {fileURLToPath, URL} from "node:url";
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  server: {
+    port: 3000,
+  },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+
+    },
+  },
+});
