4b3e1fc0d7d9ec4d6a5ee3efcacb4ec1499438ac
Taha Zoabi <zoabi03@gmail.com>
2025-01-02 14:44:58 +0200

    education

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import AboutMe from "@/components/AboutMe.vue";
 import MySkills from "@/components/MySkills.vue";
+import MyEducation from "@/components/MyEducation.vue";
 </script>
 
 <template>
   <main class="p-6">
     <AboutMe />
     <MySkills />
+    <MyEducation />
   </main>
 </template>
---
+++ src/components/MyEducation.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col justify-center gap-4 mt-12 p-3">
+    <h4 class="text-muted-foreground text-2xl font-semibold mb-2">Education</h4>
+    <div class="flex items-center gap-3 border-l-2 p-3">
+      <img
+        src="/college.png"
+        alt="College Logo"
+        class="rounded-full w-[100px] h-[100px] mr-3"
+      />
+
+      <div class="flex flex-col gap-3 p-1">
+        <h3 class="text-md">Software Practical Engineering</h3>
+        <p class="text-sm">Technion Israel Institute of Technology</p>
+        <p class="text-xs text-muted-foreground">January 2024 - Present</p>
+      </div>
+    </div>
+  </div>
+</template>
