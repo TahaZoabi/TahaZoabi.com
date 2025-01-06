f4843bbab0177da7e181065b97eb36f126f9a3b6
Taha Zoabi <zoabi03@gmail.com>
2025-01-06 10:48:01 +0200

    styles

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -4,46 +4,64 @@ import { Github, ExternalLink } from "lucide-vue-next";
 </script>
 
 <template>
-  <div class="flex flex-col justify-center gap-4 mt-12 p-3 border-l-2 p-3">
+  <div class="flex flex-col justify-center gap-6 mt-12 p-3">
     <h4 class="text-muted-foreground text-2xl font-semibold mb-2">
       My Projects
     </h4>
 
-    <div v-for="project in projects.reverse()" :key="project.id">
-      <div><img :src="project.img" alt="Project Image" /></div>
-      <h3>{{ project.title }}</h3>
-      <ul>
-        <li v-for="desc in project.description" :key="desc">
-          {{ desc }}
-        </li>
-      </ul>
-      <ul class="flex flex-wrap items-center gap-2">
-        <li
-          v-for="tool in project.tools"
-          :key="tool"
-          class="bg-secondary text-xs font-medium px-4 py-2 rounded-full shadow-sm"
-        >
-          {{ tool }}
-        </li>
-      </ul>
+    <div
+      v-for="project in projects.reverse()"
+      :key="project.id"
+      class="flex flex-col gap-6 mb-8 border-l-2 p-2"
+    >
+      <div class="w-full p-2">
+        <img
+          :src="project.img"
+          alt="Project Image"
+          class="w-full md:w-96 h-64 object-cover rounded-lg shadow-md"
+        />
+      </div>
+      <div class="flex flex-col w-full md:w-2/3 gap-4 p-2">
+        <h3 class="text-lg md:text-2xl font-semibold mb-2">
+          {{ project.title }}
+        </h3>
+        <ul class="space-y-2 list-disc pl-5">
+          <li
+            v-for="desc in project.description"
+            :key="desc"
+            class="text-muted-foreground text-sm"
+          >
+            {{ desc }}
+          </li>
+        </ul>
+        <ul class="flex flex-wrap items-center gap-2 p-2">
+          <li
+            v-for="tool in project.tools"
+            :key="tool"
+            class="bg-secondary text-xs font-medium px-4 py-2 rounded-full shadow-sm"
+          >
+            {{ tool }}
+          </li>
+        </ul>
 
-      <div class="flex items-center gap-4">
-        <a
-          :href="project.sourceCode"
-          target="_blank"
-          class="hover:text-primary flex items-center gap-2"
-        >
-          <Github />
-          View Code
-        </a>
-        <a
-          :href="project.liveDemo"
-          target="_blank"
-          class="hover:text-primary flex items-center gap-2"
-        >
-          <ExternalLink />
-          Live Demo
-        </a>
+        <div class="flex items-center gap-4 text-sm p-2">
+          <a
+            :href="project.sourceCode"
+            target="_blank"
+            class="hover:text-primary flex items-center gap-2"
+          >
+            <Github />
+            View Code
+          </a>
+          <a
+            :href="project.liveDemo"
+            target="_blank"
+            class="hover:text-primary flex items-center gap-2"
+          >
+            <ExternalLink />
+            Live Demo
+          </a>
+        </div>
       </div>
     </div>
   </div>
