97c9048ed686d037ba96b7d0670d18274a53acfe
Taha Zoabi <zoabi03@gmail.com>
2025-01-06 10:24:36 +0200

    scroll bar

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@ import InfoCard from "@/components/InfoCard.vue";
   <main
     class="flex flex-col md:flex-row gap-4 items-center bg-background max-w-[1200px] mx-auto"
   >
-    <div class="w-full md:w-1/2">
+    <div class="w-full md:w-1/3 flex-shrink-0">
       <ProfileCard />
     </div>
-    <div class="w-full md:w-2/3">
+    <div class="w-full md:w-2/3 md:overflow-y-auto md:max-h-[80vh]">
       <InfoCard />
     </div>
   </main>
