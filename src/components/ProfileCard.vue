405c61e87d6e57d272390a1649a5e22b957ba8b1
Taha Zoabi <zoabi03@gmail.com>
2025-01-19 10:30:02 +0200

    update css

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -9,7 +9,7 @@ import Resume from "/Resume.pdf";
 <template>
   <section class="flex flex-col gap-5 items-center justify-center w-full mb-6">
     <div class="p-6 max-w-screen-sm mx-auto">
-      <div class="flex justify-between items-center p-4 gap-4">
+      <div class="flex justify-between items-center gap-4">
         <img
           class="mb-4 relative w-[150px] h-[150px] overflow-hidden"
           src="/personal-pic.png"
@@ -23,15 +23,15 @@ import Resume from "/Resume.pdf";
           Software Engineering Student
         </h5>
         <div class="flex justify-center items-center gap-2 mb-4">
-          <MapPin />
-          <p class="text-muted-foreground text-md">Nein, Israel</p>
+          <MapPin class="size-5" />
+          <p class="text-md">Nein, Israel</p>
         </div>
         <p class="text-muted-foreground text-sm lg:leading-relaxed">
           Driven to build projects that expand my skills and master the latest
           in software engineering.
         </p>
       </header>
-      <div class="flex items-center p-2 gap-10">
+      <div class="flex items-center justify-start gap-10 w-full">
         <Button
           class="flex justify-center items-center gap-2"
           variant="outline"
@@ -39,7 +39,7 @@ import Resume from "/Resume.pdf";
           <Download />
           <a :href="Resume" download="TahaZoabi.pdf"> Resume </a>
         </Button>
-        <ul class="flex justify-center items-center gap-3 p-3">
+        <ul class="flex justify-center items-center gap-3 p-1">
           <li
             v-for="socialMedia in socialLinks"
             :key="socialMedia.id"
